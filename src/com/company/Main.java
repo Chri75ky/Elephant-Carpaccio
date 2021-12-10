4adccf41e4cab97075e301ba7c02914849a3f6f3
chri75ky <chri75ky@stud.kea.dk>
2021-12-10 11:38:39 +0100

    first

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,7 +10,16 @@ public class Main {
         System.out.println("Indtast antal varer og pris!");
         int amount = in.nextInt();
         int price = in.nextInt();
-        double finalPrice = (amount * price) + (amount * price * 0.0685);
+        in.nextLine();
+        System.out.println("Giv os statskoden:");
+        String stateCode = in.nextLine();
+        double finalPrice = 0;
+        if (stateCode.equalsIgnoreCase("UT")) {
+            finalPrice = (amount * price) + (amount * price * 0.0685);
+        } else if (stateCode.equalsIgnoreCase("NV")) {
+            finalPrice = (amount * price) + (amount * price * 0.08);
+        }
+
         System.out.println(finalPrice);
 
     }
