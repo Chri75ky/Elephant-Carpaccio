eaed636f46a23e99623564259e5b7301db920798
chri75ky <chri75ky@stud.kea.dk>
2021-12-10 11:32:16 +0100

    first

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello World! :)");
+        System.out.println("Indtast antal varer og pris!");
+        int amount = in.nextInt();
+        int price = in.nextInt();
+        double finalPrice = (amount * price) + (amount * price * 0.0685);
+        System.out.println(finalPrice);
+
     }
 }
--- src/com/company/UI.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class UI {
-
-
-    public void UI() {
-        System.out.println("Hello World! :)");
-    }
-}
